3e54165df237ae98c7d542af1d81b8ffa66136ac
i.bikmaev <koshakk5@gmail.com>
2020-05-16 18:49:43 +0500

    init project

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+
+declare interface RecipeInterface {
+    name: string
+    id: string
+    imageUrl: string
+    created_at: string
+    instruction: string
+    reciepeUrl: string
+    ingredients: string
+}
---
+++ pages_/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import useSWR from 'swr';
+import { request } from 'graphql-request';
+import useTranslation from 'next-translate/useTranslation';
+import Link from 'next-translate/Link';
+
+
+const API = "https://bikmay-recipes.herokuapp.com/v1/graphql";
+const fetcher = query => request(API, query);
+
+function Home () {
+    const { data, error } = useSWR(`
+        {
+            recipes(order_by: {created_at: asc}) {
+                id
+                name
+                imageUrl
+            }
+        }
+    `, fetcher);
+
+    const { t, lang } = useTranslation();
+
+    return (
+        <div>
+            <p>{t('home:title')}</p>
+            <Link href="/" lang={lang === 'en' ? 'ru' : 'en'}>{lang !== 'en' ? 'Английский' : 'Russian'}</Link>
+            <div>
+                {!data ? (
+                    <p>{t('common:loading')}</p>
+                ) : (
+                    <>
+                        {data.recipes.map((recipe: RecipeInterface) => (
+                            <div>{recipe.name}</div>
+                        ))}
+                    </>
+                )}
+            </div>
+        </div>
+    )
+};
+
+export default Home;
