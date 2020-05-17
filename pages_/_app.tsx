e9865162cf04ccdd21e53269081e439cda47c881
i.bikmaev <koshakk5@gmail.com>
2020-05-17 16:19:24 +0500

    change theme color

--- pages_/_app.tsx
+++ pages_/_app.tsx
@@ -16,7 +16,7 @@ function App ({ Component, pageProps }) {
                 <link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
                 <link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
                 <link rel="apple-touch-icon" href="/icons/apple-icon.png"></link>
-                <meta name="theme-color" content="#317EFB"/>
+                <meta name="theme-color" content="#001529"/>
             </Head>
             <Component {...pageProps} />
         </>
